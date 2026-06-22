663483f27edf509df674af490fc2035d76e37f3b
jens <mootjekalash2009@gmail.com>
2026-06-22 16:12:10 +0200

    fix leer-methodus page

---
+++ app/samenvattingen/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function LeerMethodusPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-[#181818] text-white">
+      <h1 className="text-3xl font-bold">
+        samenvattingen
+    
+      </h1>
+    </main>
+  );
+}
